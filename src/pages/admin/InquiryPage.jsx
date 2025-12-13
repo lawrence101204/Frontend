650c88e8f1025335018d7233e9746441527ba666
Cres15 <23-10727@g.batstate-u.edu.ph>
2025-12-13 09:49:36 +0800

    Day 17 cresmarie

--- src/pages/admin/InquiryPage.jsx
+++ src/pages/admin/InquiryPage.jsx
@@ -4,3 +4,11 @@ import {
   deleteInquiry as deleteInquiryMock,
 } from "../../mock/data.js";
 
+  return (
+     <button
+                onClick={() => deleteInquiry(inq.id)}
+                className="px-4 py-1 rounded-full border border-red-300 text-red-600 hover:bg-red-50 text-xs"
+              >
+                Delete
+              </button>
+  )
