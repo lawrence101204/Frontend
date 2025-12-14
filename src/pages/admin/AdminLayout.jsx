1d103ed4397b9a3fe6f97ab56b39b6a9fc150564
CatherineJoyRiosa <catherinejoyriosa@gmail.com>
2025-12-14 14:30:41 +0800

    Day 9 - Create grid layout for the package section

--- src/pages/admin/AdminLayout.jsx
+++ src/pages/admin/AdminLayout.jsx
@@ -1,19 +1,13 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import PackagePage from "./PackagePage.jsx";
-import InquiryPage from "./InquiryPage.jsx";
 
 export default function AdminLayout() {
   const [tab, setTab] = useState("package");
-  const navigate = useNavigate();
-  };
-
   return (
     <main className="px-6">
       <div className="flex items-center justify-between mb-4">
         <div className="flex gap-2">
           <button
-            onClick={() => setTab("package")}
             className={`px-5 py-2 rounded-full text-sm font-medium ${
               tab === "package"
                 ? "bg-[#d3ebd7] text-gray-800"
@@ -22,17 +16,21 @@ export default function AdminLayout() {
           >
             Package
           </button>
-          <button className="px-4 py-2 rounded-full border border-gray-400 text-sm hover:bg-gray-50">
-             Inquiry
-        </button>
-        </div>
+          <button
+            className={`px-5 py-2 rounded-full text-sm font-medium 
 
+                bg-[#d3ebd7] text-gray-800
+                bg-white text-gray-600 border border-gray-200
+            `}
+          >
+            Inquiry
+          </button>
+        </div>
         <button className="px-4 py-2 rounded-full border border-gray-400 text-sm hover:bg-gray-50">
-            Sign out
+          Sign out
         </button>
       </div>
-
-      {tab === "package" ? <PackagePage /> : <InquiryPage />}
+      {tab === "package" ? <PackagePage /> : <div>Inquiry Section</div>}
     </main>
   );
 }
